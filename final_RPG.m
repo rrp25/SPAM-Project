310399616c2fdd98a99541c8c5aa9f5bb30be516
rpg3 <56235144+rpg3@users.noreply.github.com>
2021-12-02 23:07:08 -0500

    Add files via upload

--- final_RPG.m
+++ final_RPG.m
@@ -1,13 +1,8 @@
-RGB = imread('braille.png');
+RGB = imread('alphabet2.JPG');
 [rows, columns, numberOfColorChannels] = size(RGB);
 
 % Take the red channel.
 grayImage = RGB(:, :, 1);
-% % Erase the first 110 columns to get rid of the holes.
-% verticalProfile = mean(grayImage, 2);
-% for col = 1 : 1
-% 	grayImage(:, col) = verticalProfile;
-% end
 
 % Run a texture filter on it.
 stdImage = stdfilt(grayImage);
@@ -18,34 +13,59 @@ binaryImage = bwareafilt(binaryImage, [70, inf]); % Keep only if 70 pixels or la
 
 % Find the bounding box.
 [blobRows, blobColumns] = find(binaryImage);
+
+% If there are not enough pixels before or after the cropped image, stop
+% function to prevent inaccurate translation
+if min(blobColumns)-31 < 0
+    fprintf('The image does not have enough space before the first character for the bounding box')
+    return
+elseif max(blobColumns)+31 > length(RGB) 
+    fprintf('The image does not have enough space after the last character for the bounding box')
+    return
+else
 row1 = min(blobRows);
 row2 = max(blobRows);
-col1 = min(blobColumns);
-col2 = max(blobColumns);
+col1 = min(blobColumns)-31; % see note below
+col2 = max(blobColumns)+31;   
+end
 
 % Crop it.
 croppedImage = RGB(row1:row2, col1:col2, :);
 % Convert to gray and use for display (Gray Cropped Image = GCI)
-GCI = rgb2gray(croppedImage);
+GCI = croppedImage;
 
 
 len = length(GCI);
-interval = len/6;
-I2 = imcrop(GCI,[0 0 interval 204]);
-x = 10;
-I7 = imcrop(GCI,[interval 1.51 interval 2*interval]);
-I8 = imcrop(GCI,[2*interval 1.51 interval 3*interval]);
-I9 = imcrop(GCI,[3*interval 1.51 interval 4*interval]);
-I10 = imcrop(GCI,[4*interval 1.51 interval 5*interval]);
-I11 = imcrop(GCI,[5*interval 1.51 interval 6*interval]);
+characters = round(len/180);
+interval = len/characters;
 
-h = ones(5,5) / 25;
-I3 = imfilter(I2,h);
-I4 = imadjust(I3);
-I5 = imcomplement(I4);
-se = strel('disk',1);
-I6 = imdilate(I5,se);
+figure
+hold on
+for k = 1:characters
+    
+    cropped_char1 = imcrop(GCI,[0 0 interval size(GCI,[1])]);
+    subplot(4,4,3);
+    imshow(cropped_char1);
+    cropped_char2 = imcrop(GCI,[(k)*interval 1.51 interval (k+1)*interval]);
+    subplot(4,4,k+3);
+	imshow(cropped_char2);
+    
+end
 
+
+% using imdistline to determine the distance between two characters
+% this value used for determining bounding box width on initial crop
+% distance = 64 pixels
+
+% figure
+% imshow(GCI)
+% axis on;
+% hp = impixelinfo(); % Show x,y and RGB as you mouse around.
+% h = imdistline;
+% fcn = makeConstrainToRectFcn('imline',get(gca,'XLim'),get(gca,'YLim'));
+% setDragConstraintFcn(h,fcn); 
+% title('Gray Cropped Image')
+% hold off
 
 
 subplot(4,4,1)
@@ -57,27 +77,4 @@ title('Original Color Image')
 subplot(4,4,2)
 imshow(GCI)
 axis on;
-hp = impixelinfo(); % Show x,y and RGB as you mouse around.
 title('Gray Cropped Image')
-subplot(4,4,3)
-imshow(I2)
-title('Cropped Image')
-subplot(4,4,4)
-imshow(I3)
-title('filtered Image')
-subplot(4,4,5)
-imshow(I4)
-title('contrast Image')
-subplot(4,4,6)
-imshow(I5)
-title('complement Image')
-subplot(4,4,7)
-imshow(I7)
-subplot(4,4,8)
-imshow(I8)
-subplot(4,4,9)
-imshow(I9)
-subplot(4,4,10)
-imshow(I10)
-subplot(4,4,11)
-imshow(I11)
