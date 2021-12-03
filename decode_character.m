b436fbb70ece91cea0b5e0f6cedb8d16ccd431b3
rpg3 <56235144+rpg3@users.noreply.github.com>
2021-12-03 18:16:46 -0500

    Update decode_character.m

--- decode_character.m
+++ decode_character.m
@@ -1,114 +1,121 @@
-function[output] = decode_character(characters,cropped_char,threshold)
+function[output] = decode_character(rows,interval,characters,cropped_char,threshold)
 
-boxcc = zeros(1,6);
 numBlackPixelscc = zeros(1,6);
 
 for k = 1:characters-1
 
-characterk = cropped_char{k};
-upperLeftcc = characterk(1:69,1:90,:);
-upperRightcc = characterk(1:69,91:179,:);
-middleLeftcc = characterk(70:139,1:90,:);
-middleRightcc = characterk(70:139,91:179,:);
-bottomLeftcc = characterk(140:208,1:90,:);
-bottomRightcc = characterk(140:208,91:179,:);
+row1 = round(rows/3);
+row2 = round(rows*2/3);
+row3 = rows;  
+col1 = round(interval/2);
+col2 = interval;
+    
+characterk = cropped_char(:,:,k);
+upperLeftcc = characterk(1:row1,1:col1);
+upperRightcc = characterk(1:row1,col1:col2);
+middleLeftcc = characterk(row1:row2,1:col1);
+middleRightcc = characterk(row1:row2,col1:col2);
+bottomLeftcc = characterk(row2:row3,1:col1);
+bottomRightcc = characterk(row2:row3,col1:col2);
 
 numBlackPixelscc(1) = sum(upperLeftcc == 0,'all');
-numBlackPixelscc(3) = sum(upperRightcc == 0,'all');
-numBlackPixelscc(5) = sum(middleLeftcc == 0,'all');
-numBlackPixelscc(2) = sum(middleRightcc == 0,'all');
-numBlackPixelscc(4) = sum(bottomLeftcc == 0,'all');
+numBlackPixelscc(4) = sum(upperRightcc == 0,'all');
+numBlackPixelscc(2) = sum(middleLeftcc == 0,'all');
+numBlackPixelscc(5) = sum(middleRightcc == 0,'all');
+numBlackPixelscc(3) = sum(bottomLeftcc == 0,'all');
 numBlackPixelscc(6) = sum(bottomRightcc == 0,'all');
 
-
+boxcc = zeros(1,6);
     for i = 1:6
         if numBlackPixelscc(i) > threshold
             boxcc(i) = 1;
         end
+ 
     end
     
     
     
+    
 if boxcc == [1 0 0 0 0 0] 
-    output = "A";
+    output(k) = "A";
     
 elseif boxcc == [1 1 0 0 0 0]
-    output = "B";
+    output(k) = "B";
     
 elseif boxcc == [1 0 0 1 0 0]
-    output = "C";
+    output(k) = "C";
 
 elseif boxcc == [1 0 0 1 1 0]
-    output = "D";
+    output(k) = "D";
 
 elseif boxcc == [1 0 0 0 1 0]
-    output = "E";
+    output(k) = "E";
     
 elseif boxcc == [1 1 0 1 0 0]
-    output = "F";
+    output(k) = "F";
 
 elseif boxcc == [1 1 0 1 1 0]
-    output = "G";
+    output(k) = "G";
 
 elseif boxcc == [1 1 0 0 1 0]
-    output = "H";
+    output(k) = "H";
 
 elseif boxcc == [0 1 0 1 0 0]
-    output = "I";
+    output(k) = "I";
 
 elseif boxcc == [0 1 0 1 1 0]
-    output = "J";
+    output(k) = "J";
 
 elseif boxcc == [1 0 1 0 0 0]
-    output = "K";
+    output(k) = "K";
 
 elseif boxcc == [1 1 1 0 0 0]
-    output = "L";
+    output(k) = "L";
 
 elseif boxcc == [1 0 1 1 0 0]
-    output = "M";
+    output(k) = "M";
 
 elseif boxcc == [1 0 1 1 1 0]
-    output = "N";
+    output(k) = "N";
     
 elseif boxcc == [1 0 1 0 1 0]
-    output = "O";
+    output(k) = "O";
 
 elseif boxcc == [1 1 1 1 0 0]
-    output = "P";
+    output(k) = "P";
 
 elseif boxcc == [1 1 1 1 1 0]
-    output = "Q";
+    output(k) = "Q";
 
 elseif boxcc == [1 1 1 0 1 0]
-    output = "R";
+    output(k) = "R";
 
 elseif boxcc == [0 1 1 1 0 0]
-    output = "S";
+    output(k) = "S";
 
 elseif boxcc == [0 1 1 1 1 0]
-    output = "T";
+    output(k) = "T";
 
 elseif boxcc == [1 0 1 0 0 1]
-    output = "U";
+    output(k) = "U";
 
 elseif boxcc == [1 1 1 0 0 1]
-    output = "V";
+    output(k) = "V";
 
 elseif boxcc == [0 1 0 1 1 1]
-    output = "W";
+    output(k) = "W";
 
 elseif boxcc == [1 0 1 1 0 1]
-    output = "X";
+    output(k) = "X";
     
 elseif boxcc == [1 0 1 1 1 1]
-    output = "Y";
+    output(k) = "Y";
 
 elseif boxcc == [1 0 1 0 1 1]
-    output = "Z";
+    output(k) = "Z";
     
 elseif boxcc == [0 0 0 0 0 0]
-    output = " ";
+    output(k) = " ";
       
 end
 
