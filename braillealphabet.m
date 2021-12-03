9519ce075a7567ca7c8b7115ee4b932e48254d39
rrp25 <94181093+rrp25@users.noreply.github.com>
2021-12-02 23:13:21 -0500

    Add files via upload

---
+++ braillealphabet.m
@@ -0,0 +1,87 @@
+output = "N/A"
+box = [0 0 0 0 0 0]
+
+if box == [1 0 0 0 0 0] 
+    output = "A";
+    
+elseif box == [1 1 0 0 0 0]
+    output = "B";
+    
+elseif box == [1 0 0 1 0 0]
+    output = "C";
+
+elseif box == [1 0 0 1 1 0]
+    output = "D";
+
+elseif box == [1 0 0 0 1 0]
+    output = "E";
+    
+elseif box == [1 1 0 1 0 0]
+    output = "F";
+
+elseif box == [1 1 0 1 1 0]
+    output = "G";
+
+elseif box == [1 1 0 0 1 0]
+    output = "H";
+
+elseif box == [0 1 0 1 0 0]
+    output = "I";
+
+elseif box == [0 1 0 1 1 0]
+    output = "J";
+
+elseif box == [1 0 1 0 0 0]
+    output = "K";
+
+elseif box == [1 1 1 0 0 0]
+    output = "L";
+
+elseif box == [1 0 1 1 0 0]
+    output = "M";
+
+elseif box == [1 0 1 1 1 0]
+    output = "N";
+    
+elseif box == [1 0 1 0 1 0]
+    output = "O";
+
+elseif box == [1 1 1 1 0 0]
+    output = "P";
+
+elseif box == [1 1 1 1 1 0]
+    output = "Q";
+
+elseif box == [1 1 1 0 1 0]
+    output = "R";
+
+elseif box == [0 1 1 1 0 0]
+    output = "S";
+
+elseif box == [0 1 1 1 1 0]
+    output = "T";
+
+elseif box == [1 0 1 0 0 1]
+    output = "U";
+
+elseif box == [1 1 1 0 0 1]
+    output = "V";
+
+elseif box == [0 1 0 1 1 1]
+    output = "W";
+
+elseif box == [1 0 1 1 0 1]
+    output = "X";
+    
+elseif box == [1 0 1 1 1 1]
+    output = "Y";
+
+elseif box == [1 0 1 0 1 1]
+    output = "Z";
+    
+elseif box == [0 0 0 0 0 0]
+    output = " ";
+      
+end
+        
+        
