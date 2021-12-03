731933a1d240aa9a8e4455540537ee9f4bfe204f
rpg3 <56235144+rpg3@users.noreply.github.com>
2021-12-02 23:15:09 -0500

    Update final_RPG.m

--- final_RPG.m
+++ final_RPG.m
@@ -12,35 +12,66 @@ binaryImage = stdImage > 5;
 binaryImage = bwareafilt(binaryImage, [70, inf]); % Keep only if 70 pixels or larger.
 
 % Find the bounding box.
-[blobRows, blobColumns] = find(binaryImage);
+[dotRows, dotColumns] = find(binaryImage);
 
 % If there are not enough pixels before or after the cropped image, stop
 % function to prevent inaccurate translation
-if min(blobColumns)-31 < 0
+if min(dotColumns)-31 < 0
     fprintf('The image does not have enough space before the first character for the bounding box')
     return
-elseif max(blobColumns)+31 > length(RGB) 
+elseif max(dotColumns)+31 > length(RGB) 
     fprintf('The image does not have enough space after the last character for the bounding box')
     return
 else
-row1 = min(blobRows);
-row2 = max(blobRows);
-col1 = min(blobColumns)-31; % see note below
-col2 = max(blobColumns)+31;   
+row1 = min(dotRows);
+row2 = max(dotRows);
+col1 = min(dotColumns)-31; % see note below
+col2 = max(dotColumns)+31; % see note below
 end
+
+%% NOTE
+% We used imdistline to determine the distance between two characters.
+% This value was used for determining bounding box width on initial crop.
+% Distance = 62 pixels -> half distance for either side = 31 pixels
+
+% figure
+% imshow(GCI)
+% axis on;
+% hp = impixelinfo(); % Show x,y and RGB as you mouse around.
+% h = imdistline;
+% fcn = makeConstrainToRectFcn('imline',get(gca,'XLim'),get(gca,'YLim'));
+% setDragConstraintFcn(h,fcn); 
+% title('Cropped Image with Distance Line')
+%%
 
 % Crop it.
 croppedImage = RGB(row1:row2, col1:col2, :);
 % Convert to gray and use for display (Gray Cropped Image = GCI)
 GCI = croppedImage;
 
-
+% Calculate image length and then estimate number of characters based on 
+% known width (approx. 180 pixels). Use this to determine interval length
 len = length(GCI);
 characters = round(len/180);
 interval = len/characters;
 
+
+% Create figure to plot original image and show cropped image next to it
 figure
 hold on
+
+subplot(4,4,1)
+imshow(RGB)
+axis on;
+title('Original Color Image')
+
+subplot(4,4,2)
+imshow(GCI)
+axis on;
+title('Gray Cropped Image')
+
+% Run loop to crop each character to equal size and then plot it on same figure
+
 for k = 1:characters
     
     cropped_char1 = imcrop(GCI,[0 0 interval size(GCI,[1])]);
@@ -51,30 +82,3 @@ for k = 1:characters
 	imshow(cropped_char2);
     
 end
-
-
-% using imdistline to determine the distance between two characters
-% this value used for determining bounding box width on initial crop
-% distance = 64 pixels
-
-% figure
-% imshow(GCI)
-% axis on;
-% hp = impixelinfo(); % Show x,y and RGB as you mouse around.
-% h = imdistline;
-% fcn = makeConstrainToRectFcn('imline',get(gca,'XLim'),get(gca,'YLim'));
-% setDragConstraintFcn(h,fcn); 
-% title('Gray Cropped Image')
-% hold off
-
-
-subplot(4,4,1)
-imshow(RGB)
-axis on;
-hp = impixelinfo(); % Show x,y and RGB as you mouse around.
-title('Original Color Image')
-
-subplot(4,4,2)
-imshow(GCI)
-axis on;
-title('Gray Cropped Image')
